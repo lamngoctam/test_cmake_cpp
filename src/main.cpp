d7557cab7e79156c70db17debe7d9cde6c44f0b1
lamngoctam <lamngoctam@gmail.com>
2022-04-03 22:38:29 +0800

    main edittingh

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,47 @@
-#include <iostream>
-#include "Arithmetic.hpp"
+#include <cassert>
 
-int main(int argc, char** argv){
-	std::cout << "Hello cpp cmake and vim" << std::endl;
+#include "directed_graph/DirectedGraph.hpp"
 
-	std::cout << "10 + 15 = " << Arithmetic::add(10,15) << std::endl;
+// test from the `directed graph setup` video
+void test_DirectedGraph_setup() {
+    DirectedGraph graph;
 
-	return 0;
+    std::vector<Vertex*> vs;
+    for (int i = 0; i < 4; i++) {
+        auto* const v = graph.addVertex(i);
+        vs.push_back(v);
+    }
+
+    // v0 -> v0
+    graph.addEdge(*vs.at(0), *vs.at(0));
+    // v0 -> v1
+    graph.addEdge(*vs.at(0), *vs.at(1));
+    // v0 -> v3
+    graph.addEdge(*vs.at(0), *vs.at(3));
+    // v2 -> v3
+    graph.addEdge(*vs.at(2), *vs.at(3));
+    // v2 -> v3
+    graph.addEdge(*vs.at(2), *vs.at(3));
+
+    // compare using `DirectedGraph::toString`
+    const auto result = graph.toString();
+    const auto expected = std::string("DirectedGraph:\n")  //
+                          + "  vertices:\n"                //
+                          + "    v0\n"                     //
+                          + "    v1\n"                     //
+                          + "    v2\n"                     //
+                          + "    v3\n"                     //
+                          + "  edges:\n"                   //
+                          + "    v0 -> v0\n"               //
+                          + "    v0 -> v1\n"               //
+                          + "    v0 -> v3\n"               //
+                          + "    v2 -> v3\n";              //
+
+    assert(result == expected);
 }
+
+int main(int argc, char** argv) {
+    test_DirectedGraph_setup();
+    return 0;
+}
+
