2c71cfc5c70359f18defd06bb7afc65924b84bd6
lamngoctam <lamngoctam@gmail.com>
2022-04-03 20:51:11 +0800

    main modify

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char** argv){
 	std::cout << "Hello cpp cmake and vim" << std::endl;
 
-	std::cout << "10 + 14 = " << Arithmetic::add(10,14) << std::endl;
+	std::cout << "10 + 15 = " << Arithmetic::add(10,15) << std::endl;
 
 	return 0;
 }
